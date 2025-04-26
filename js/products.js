6b9bd3801c7e0c3ce8ac87db04dffb6780e43f8c
Grimwood Village <grimwoodvillage@gmail.com>
2025-04-26 13:55:25 -0700

    Update products.js

--- js/products.js
+++ js/products.js
@@ -13,7 +13,7 @@ export function getProduct(productId) {
 export const products = [
 {
     id: '1',
-    image: 'images/products/forgehammert.png',
+    image: 'images/products/forgehammergreen.png',
     name: 'Forge Hammer Tee',
 },
 {
