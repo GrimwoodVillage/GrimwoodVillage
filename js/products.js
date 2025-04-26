05efcce5f070f02b49ce58415556375658c649d9
Grimwood Village <grimwoodvillage@gmail.com>
2025-04-26 14:20:22 -0700

    Update products.js

--- js/products.js
+++ js/products.js
@@ -15,21 +15,25 @@ export const products = [
     id: '1',
     image: 'images/products/forgehammergreen.png',
     name: 'Forge Hammer Tee',
+    link: 'https://grimwood-village.printify.me/product/19843558/forge-hammer-tee',
 },
 {
     id: '2',
     image: 'images/products/vowreavert.png',
     name: 'Vowreaver Tee',
+    link: 'https://grimwood-village.printify.me/product/19843337/vowreaver-tee',
 },
 {
     id: '3',
     image: 'images/products/fellhowlt.png',
     name: 'Fellhowl Tee',
+    link: 'https://grimwood-village.printify.me/product/19844479/fellhowl-tee',
 },
 {
     id: '4',
     image: 'images/products/grimwoodvillaget.png',
     name: 'Grimwood Tee',
+    link: 'https://grimwood-village.printify.me/product/19843412/grimwood-village-tee',
 },
 {
     id: '5',
