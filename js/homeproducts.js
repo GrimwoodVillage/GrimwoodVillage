bd3420d11c0ff5e8d94411a28ae585e233f7d6ab
Grimwood Village <grimwoodvillage@gmail.com>
2025-04-29 15:31:21 -0700

    Update homeproducts.js

--- js/homeproducts.js
+++ js/homeproducts.js
@@ -7,9 +7,9 @@ products.slice(0, 6).forEach((product) => {
         <div class="product-box">
             <a href="${product.link}" target="_blank">
                 <img class="product-img" src="${product.image}">
+                <h2 class="product-title">${product.name}</h2>
+                <img class="cartadd" src="images/cart2.svg">
             </a>
-            <h2 class="product-title">${product.name}</h2>
-            <img class="cartadd" src="images/cart2.svg">
         </div>
     `;
 });
